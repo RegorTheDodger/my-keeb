b6bbc3e0d205251e3f18b2fb794d4e3d2b113dd8
RegorTheDodger <roger.johnsanthasingh@education.nsw.gov.au>
2026-08-17 21:33:39 +1000

    Update configuration for RegorBoard 6000 TKL

--- firmware/config.h
+++ firmware/config.h
@@ -1,29 +1,28 @@
-// RegorBoard 6000 - QMK Configuration
-// Full-size 104-key keyboard with Orpheus Pico (RP2040)
+// RegorBoard 6000 TKL - QMK Configuration
+// TKL 87-key keyboard with Orpheus Pico (RP2040)
 
 #pragma once
 
 #define MANUFACTURER    "RegorTheDodger"
-#define PRODUCT         "RegorBoard 6000"
+#define PRODUCT         "RegorBoard 6000 TKL"
 
-// Matrix size - 8 rows x 14 columns = 112 positions (104 used)
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 14
+// Matrix size - 6 rows x 17 columns = 87 keys
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 17
 
 // Orpheus Pico (RP2040) pin assignments
 // Row pins
-#define MATRIX_ROW_PINS { GP0, GP1, GP2, GP3, GP4, GP5, GP6, GP7 }
+#define MATRIX_ROW_PINS { GP0, GP1, GP2, GP3, GP4, GP5 }
 // Column pins
-#define MATRIX_COL_PINS { GP8, GP9, GP10, GP11, GP12, GP13, GP14, GP15, GP16, GP17, GP18, GP19, GP20, GP21 }
+#define MATRIX_COL_PINS { GP6, GP7, GP8, GP9, GP10, GP11, GP12, GP13, GP14, GP15, GP16, GP17, GP18, GP19, GP20, GP21, GP22 }
 
-// Diode direction (COL2ROW for standard keyboard wiring)
+// Diode direction
 #define DIODE_DIRECTION COL2ROW
 
 // USB polling rate
 #define USB_POLLING_INTERVAL_MS 1
 
-// Key debounce in milliseconds
+// Debounce
 #define DEBOUNCE 5
 
-// Silent switches - no need for extra debounce
 #define FORCE_NKRO
